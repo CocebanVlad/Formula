048168c34a027857eac5c5492315df56590d22a1
Coceban Vlad <coceban.vlad@hotmail.com>
2021-01-26 16:28:38 +0200

    rename a var

--- Xtel.PromoFormula/Xtel.PromoFormula/BuildingPipelines/FormulaBuildingPipeline.cs
+++ Xtel.PromoFormula/Xtel.PromoFormula/BuildingPipelines/FormulaBuildingPipeline.cs
@@ -7,14 +7,14 @@ namespace Xtel.PromoFormula.BuildingPipelines
     public class FormulaBuildingPipeline : BuildingPipeline
     {
         // With the StringConcat optimization turn on, the behaviour is the same as in C#
-        private const bool UseStringConcatOptimization = true;
+        private const bool ApplyStringConcatOptimization = true;
 
         protected override IList<IExprBuilder> Builders => new List<IExprBuilder>()
         {
             new ConstantExprBuilder(),
             new GroupExprBuilder(),
-            new StringConcatExprBuilder(UseStringConcatOptimization), // must be before `MathExprBuilder`
-            new MathExprBuilder(UseStringConcatOptimization),
+            new StringConcatExprBuilder(ApplyStringConcatOptimization), // must be before `MathExprBuilder`
+            new MathExprBuilder(ApplyStringConcatOptimization),
             new PlusOrMinusExprBuilder(),
             new ConditionalExprBuilder(),
             new NegationExprBuilder(),
